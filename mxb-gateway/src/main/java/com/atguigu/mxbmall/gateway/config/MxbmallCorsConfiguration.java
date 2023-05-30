8694e50e38202c7545e2899d0ac58cdaddec8632
erfenjiao <630166475@qq.com>
2023-05-30 19:17:28 +0800

    商品服务-API-三级分类-网关统一跨域

---
+++ mxb-gateway/src/main/java/com/atguigu/mxbmall/gateway/config/MxbmallCorsConfiguration.java
@@ -0,0 +1,35 @@
+package com.atguigu.mxbmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class MxbmallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration= new CorsConfiguration();
+
+        //1、配置跨域
+        // 允许跨域的头
+        corsConfiguration.addAllowedHeader("*");
+        // 允许跨域的请求方式
+        corsConfiguration.addAllowedMethod("*");
+        // 允许跨域的请求来源
+        corsConfiguration.addAllowedOrigin("*");
+        // 是否允许携带cookie跨域
+        corsConfiguration.setAllowCredentials(true);
+
+        // 任意url都要进行跨域配置
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+
+    }
+
+
+}
